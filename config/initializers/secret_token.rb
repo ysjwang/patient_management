2297131f36a0e5e9039a63b4d44d4afa0c11025c
Josiah Wang <ysjwang@gmail.com>
2013-10-30 11:03:19 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PatientManagement::Application.config.secret_key_base = 'ebe779e078ff9e4b7c0594210742ddf38afb7304c4c7f521aef40ffaa38b9a9901b3d9b32728365ef111a7970782d141f917b9769871f3bf6c2d35f69eb78b0a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PatientManagement::Application.config.session_store :cookie_store, key: '_patient_management_session'
