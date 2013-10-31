9eaba0aded7b9cf87a6da88cc71b3a8f2903e308
Josiah Wang <ysjwang@gmail.com>
2013-10-31 14:10:03 -0400

    Added seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,25 +9,33 @@
 
 # Create some Doctors
 10.times do |number|
-  create(:doctor, email: 'doctor' + number + '@email.com')
+  FactoryGirl.create(:doctor, email: 'doctor' + number.to_s + '@email.com')
 end
 
 
 # Create some more Patients
 50.times do |number|
-  create(:patient, email: 'patient' + number + '@email.com')
+  FactoryGirl.create(:patient, email: 'patient' + number.to_s + '@email.com')
 end
 
 
 # Create some Worktypes
 10.times do |number|
-  create(:worktype)
+  FactoryGirl.create(:worktype)
 end
 
 # Create some appointments
-Patient.all.each do
+Patient.all.each do |patient|
   appointment_count = rand(1..4)
-  appointment_count.each do 
+  appointment_count.times do 
+    # For each of these appointments
+
+    # Randomize if past, present, or future (bias it towards past or ongoing)
+    appointment_type = [:valid_past_appointment, :valid_past_appointment, :valid_past_appointment, :valid_ongoing_appointment, :valid_ongoing_appointment, :valid_future_appointment].sample
+    appointment = FactoryGirl.create(appointment_type, patient: patient, doctor: Doctor.all.sample)
+    rand(1..3).times do
+      appointment.works << FactoryGirl.create(:work, appointment: appointment, worktype: Worktype.all.sample)
+    end
 
   end
 
--- spec/factories/appointments.rb
+++ spec/factories/appointments.rb
@@ -15,8 +15,11 @@ FactoryGirl.define do
     end
 
     trait :past_appointment do
-      start_time { Time.at(Time.now.to_f / 2) }
-      end_time { Time.at(Time.now.to_f / 2 + 3600) } # Hour long appointment
+      week_ago = Time.now.to_f - (60 * 60 * 24 * 7).to_f
+      rand_day_adjuster = (60 * 60 * 24 * Random.rand(1..5)).to_f
+      rand_hour_adjuster = (60 * 60 * Random.rand(1..20)).to_f
+      start_time { Time.at(week_ago - rand_day_adjuster - rand_hour_adjuster) }
+      end_time { Time.at(week_ago - rand_day_adjuster - rand_hour_adjuster + 3600) } # Hour long appointment
     end
 
     trait :ongoing_appointment do
@@ -25,13 +28,16 @@ FactoryGirl.define do
     end
 
     trait :future_appointment do
-      start_time { Time.at(Time.now.to_f * 2) }
-      end_time { Time.at(Time.now.to_f * 2 + 3600) } # Hour long appointment
+      week_ahead = Time.now.to_f + (60 * 60 * 24 * 7)
+      rand_day_adjuster = 60 * 60 * 24 * Random.rand(1..5)
+      rand_hour_adjuster = 60 * 60 * Random.rand(1..20)
+      start_time { Time.at(week_ahead + rand_day_adjuster + rand_hour_adjuster) }
+      end_time { Time.at(week_ahead + rand_day_adjuster + rand_hour_adjuster + 3600) } # Hour long appointment
     end
 
     trait :with_works do
       after(:create) do |appointment|
-        rand(2..4).times do
+        Random.rand(2..4).times do
           appointment.works << build(:work, appointment: appointment)
         end
       end
